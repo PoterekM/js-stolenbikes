69a45e66f35fdcb0158175563d4b2afb8956a7ff
Max Scher <maxobaxo@gmail.com>
2017-08-02 23:14:08 -0700

    attempt to successfully parse JSON and append information to index.html, unsuccessful

--- js/bike.js
+++ js/bike.js
@@ -1,6 +1,27 @@
-// function Bike(manufacturer, locations) {
-//   this.manufacturer = manufacturer;
-//   this.locations = locations;
-// }
-//
-// exports.bikeModule = Bike;
+function BikeList() {
+  this.bikeArr = [];
+};
+
+BikeList.prototype.getBikes = function() {
+  $.get('https://bikeindex.org/api/v3/search?locations=' + manufacturer)
+  .then(function(response) {
+    bikeReturn = response.bikes;
+    for (i = 0; i < bikeReturn.length; i++) {
+      this.bikeArr.push(bikeReturn[i]);
+    }
+  });
+};
+
+BikeList.prototype.displayBikes = function() {
+  for (i = 0; i < this.BikeArr.length; i++) {
+    var newBike = this.BikeArr[i];
+    var bikeTitle = newBike.title;
+
+  }
+};
+
+
+
+
+
+exports.bikeModule = Bike;
